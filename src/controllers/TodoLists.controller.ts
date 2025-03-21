1fef14c61897c112729e111f7bd8929f98edbc95
khang <enone859@gmail.com>
2025-03-20 22:32:49 -0400

    Add TodoListsController for creating todo lists
    - Introduced TodoListsController with a create method to handle the creation of new todo lists.
    - Integrated database query to insert new lists and return appropriate JSON responses based on success or failure.

---
+++ src/controllers/TodoLists.controller.ts
@@ -0,0 +1,16 @@
+import { Context } from "hono";
+import { db } from "@/services/Database.service";
+
+export class TodoListsController {
+  static async create(c: Context) {
+    const { name } = await c.req.json();
+
+    const list = db.query(`INSERT INTO lists (name) VALUES (?);`).run(name);
+
+    if (!list) {
+      return c.json({ error: "Failed to create list" }, 500);
+    }
+
+    return c.json({ message: "Created list successfully" });
+  }
+}
